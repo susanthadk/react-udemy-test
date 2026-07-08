1e166a60eaf2bc371477da80f22b4f59e2ac30e9
Susantha Kumara <dksusantha@outlook.com>
2026-07-08 11:42:18 +0530

    change subheading

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import "./App.css";
 
 function App() {
-  return <h1>Learn React with Vite</h1>;
+  return (
+    <>
+      <h1>Learn React with Vite</h1>
+      <p>I would love to change this.</p>
+    </>
+  );
 }
 
 export default App;
